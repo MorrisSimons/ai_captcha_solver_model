839db900e783683d924928cf42e09324383244a7
Morris Simons <N1ghtmare1@users.noreply.github.com>
2022-12-06 19:14:22 +0100

    new data plus classification started

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import hashlib
 import os
 import albumentations
+import torch
+import numpy as np
+from PIL import Image
+from PIL import ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+class Classification:
+    def __init__(self, image_path, targets, resize=None):
+        self.image_path = image_path
+        self.targets = targets
+        self.resize = resize
+        self.aug = albumentations.Compose([albumentations.Normalize(always_apply=True)])
+    
+    def len(self):
+        return len(self.image_path)
+    
+    def __getitem__(self, item):
+        image = Image.open(self.image_path[item].convert("RGB"))
+        targets = self.targets[item]
+
+        if self.resize is not None:
+            image = image.resize((self.resize[1], self.resize[0]), resample=Image.BILINEAR)
+        image = np.array(image)
+
 
 
 def get_files(path):
