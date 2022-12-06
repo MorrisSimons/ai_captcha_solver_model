fba2dfa0e09a2f8c3cd168d2d83f208ea289f79e
Morris Simons <N1ghtmare1@users.noreply.github.com>
2022-12-06 19:24:13 +0100

    Classification done

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
 discord_webhookurl = "https://discord.com/api/webhooks/1049452576233500733/0RH04CSndynT4fv8ONAF3glEiTPSenCW_R40MNTuNYjBVCY_4TnDoIOb05XpphVm2NWM"
+
+DATADIR = "../input/captcha_images_v2/"
+BATCH_SIZE = 8
+IMAGE_WIDTH = 300
+IMAGE_HEIGHT = 100
+NUM_WORKERS = 8
+EPOCHS = 200
+DEVICE = "cuda"
---
+++ train.py
@@ -0,0 +1,11 @@
+import os
+import glob
+import torch
+import numpy as np
+
+from sklearn import preprocessing
+from sklearn import model_selection
+from sklearn import metrics
+
+import config
+import dataset
